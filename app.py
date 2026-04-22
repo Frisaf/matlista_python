24ce826e1b06a53fef608abd7d87a66b11ad886e
Frisaf <fridasafsten@gmail.com>
2026-04-22 13:51:40 +0200

    started adding classes for generator and dish, as well as functions for them both

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-import asyncio
+import asyncio, random
 from prisma import Prisma
 
+class Generator:
+    def __init__(self, db) -> None:
+        self.db = db
+    
+    def get_random_dish(self, dishes) -> object:
+        dish = random.choice(dishes)
+        return dish
+    
+    def find_same(self, dish, dishes) -> list:
+        name = dish.name
+        result = []
+
+        for dish in dishes:
+            if dish.name == name:
+                result.append(dish)
+        
+        return result
+    
 class Dish:
     def __init__(self, name, main, side, other_info, weekend_worthy) -> None:
         self.name = name
@@ -8,14 +26,27 @@ class Dish:
         self.side = side
         self.other_info = other_info
         self.weekend_worthy = weekend_worthy
+    
+    def print_info(self, dish):
+        return print(f"{dish.name.capitalize()}\nHuvudingrediens: {dish.main}\nTillbehör: {dish.sides}\nÖvrig info: {dish.otherInfo}")
 
 async def main() -> None:
     db = Prisma()
+    generator = Generator(db)
+
     await db.connect()
 
-    dishes = await db.dishes.find_many()
+    dishes = await db.dishes.find_many(
+        include = {
+            "main": True,
+            "side": True
+        }
+    )
+    
+    random_dish = generator.get_random_dish(dishes)
+    all_dishes = generator.find_same(random_dish, dishes)
 
-    print(type(dishes[0]))
+    print(dishes)
 
 if __name__ == '__main__':
     asyncio.run(main())
